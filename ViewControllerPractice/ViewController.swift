0f767d04ca96aca2f353ea3ec6a7f8c43f4c7adc
James D Vieyra <james@vieyra.co.za>
2022-04-11 09:40:43 +0200

    Initial Commit

---
+++ ViewControllerPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewControllerPractice
+//
+//  Created by IACD-011 on 2022/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
