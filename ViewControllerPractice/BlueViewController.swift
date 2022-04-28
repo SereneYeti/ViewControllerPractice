79f94412d5deeb324bafe729deade59f55b02c53
James D Vieyra <james@vieyra.co.za>
2022-04-28 11:14:43 +0200

    Simple View Controller Practice

---
+++ ViewControllerPractice/BlueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BlueViewController.swift
+//  ViewControllerPractice
+//
+//  Created by IACD-011 on 2022/04/11.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Blue Screen"
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
